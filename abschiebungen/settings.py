37180d337951cbac50475cfdf4332e1cb7ee7356
Magdalena Noffke <noffke@mi.fu-berlin.de>
2014-08-28 09:26:48 +0200

    changes on models

--- abschiebungen/settings.py
+++ abschiebungen/settings.py
@@ -42,15 +42,12 @@ ROOT_URLCONF = 'abschiebungen.urls'
 WSGI_APPLICATION = 'abschiebungen.wsgi.application'
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db/abschiebungen.sqlite'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'abschiebungen'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 LANGUAGE_CODE = 'de-de'
 
